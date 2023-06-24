eb2ba7fdbe352fde638ee1a2b0c474c33bc8fb8d
Matthew Mitchell <mazchoo@gmail.com>
2023-06-24 23:41:22 +0700

    Added ToDo

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from dash_bootstrap_components.themes import BOOTSTRAP
 from src.components.layout import create_layout
 from src.language import __t__
 
+# ToDo - Make a data source class that interacts directly with the data
 
 def main():
     app = Dash(external_stylesheets=[BOOTSTRAP])
